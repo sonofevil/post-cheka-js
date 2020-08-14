fd563fccb3255f57ebec646efe1010f97552189a
sonofevil <debilerpc@web.de>
2020-08-14 15:01:45 +0200

    can now be used on user profiles in mobile browser

--- Reddit_Post_Cheka.user.js
+++ Reddit_Post_Cheka.user.js
@@ -8,6 +8,9 @@
 // @downloadURL	https://github.com/sonofevil/post-cheka-js/raw/master/Reddit_Post_Cheka.user.js
 // @version     1.1
 // @grant       none
+// @run-at      document-idle
+// @require     https://code.jquery.com/jquery-2.1.4.min.js
+// @require     https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
 // ==/UserScript==
 
 
@@ -105,31 +108,32 @@ function PostCheka($){
 	self.blacklist = [];
 	self.users = {};
 	self.window;
-	
-	
+
+
 	// call constructor
 	init();
-	
-	
+
+
 	// constructor
 	function init(){
 		self.window = new ReviewWindow(self);
-		
+
 		init_html();
+        waitForKeyElements (".UserProfileHeader__banner-user-name", init_html2);
 		init_events();
-		
+
 		// compile blacklist regex
 		$.each(BLACKLIST, function(i, v){
 			self.blacklist.push(new RegExp(v, "ig"));
 		});
 	}
-	
+
 	// initialize html and css
 	function init_html(){
 		// create [R] buttons next to usernames
 		var button = $("<a class='cheka-button' href='###' title='Review post history' style='margin-left:3px;font-weight:bold;text-decoration:underline overline'>| Scan for Keywords |</a>");
 		if (button.is("div")) button = $(button.get(1)); // fix for a weird bug where <a> gets wrapped in a <div>
-		
+
 		$(".tagline").each(function(){
 			var user = $(this).text().trim();
 			if (!user || user == "[deleted]") return;
@@ -137,23 +141,39 @@ function PostCheka($){
 			$(this).parent().find(".author").after(b);
 		});
 	}
-	
+
+    // initialize html and css on mobile reddit
+    function init_html2(){
+		// create [R] buttons next to usernames
+		var button = $("<a class='cheka-button' href='###' title='Review post history' style='margin-left:3px;font-weight:bold;font-size:24;text-decoration:underline overline'>| SCAN |</a>");
+		if (button.is("div")) button = $(button.get(1)); // fix for a weird bug where <a> gets wrapped in a <div>
+
+		$(".UserProfileHeader__banner").each(function(){
+			var user = $(this).text().trim();
+			if (!user || user == "[deleted]") return;
+			var b = button.clone();
+			$(this).parent().find(".UserProfileHeader__banner-user-name").after(b);
+		});
+	}
+
 	function init_events(){
 		// review button click
 		$(document).delegate(".cheka-button", "click", function(e){
 			e.preventDefault();
 			var username = $(this).parent().find(".author").text().trim();
+            if (!username) username = $(this).parent().find(".UserProfileHeader__banner-user-name").text().trim();
+            username = username.replace(/^u\//, '');
 			if (!username || username == "[deleted]"){
 				alert("Invalid user to review");
 				return;
 			}
-			
+
 			var user = new User(username, self);
 			self.users[user.name] = user;
 			self.window.open();
 			self.window.review(user);
 		});
-		
+
 		// esc key
 		$(window).keydown(function(e){
 			if (e.which == 27){
@@ -175,9 +195,9 @@ function User(name, core){
 	self.matched_words = {};
 	self.getting_data = false;
 	self.fetch_complete = false;
-	
+
 	self.add_word = add_word;
-	
+
 	function add_word(word){
 		word = word.toLowerCase();
 		if (!self.matched_words[word]) self.matched_words[word] = 0;
@@ -201,11 +221,11 @@ function Post(data){
 	self.timestamp = "";
 	self.time_ago = "";
 	self.points = 0;
-	
+
 	self.add_word = add_word;
-	
+
 	function add_word(word){
-		if (self.matches.indexOf(word.toLowerCase()) < 0) self.matches.push(word.toLowerCase());		
+		if (self.matches.indexOf(word.toLowerCase()) < 0) self.matches.push(word.toLowerCase());
 	}
 }
 
@@ -222,24 +242,24 @@ function ReviewWindow(core){
 	self.mode = MODE_LOADING;
 	self.element;
 	self.post_template;
-	
+
 	self.open = open;
 	self.close = close;
 	self.is_open = is_open;
 	self.log = log;
 	self.review = review;
-	
-	
+
+
 	init();
-	
-	
+
+
 	function review(user){
 		// review user's post history up to 1000 posts back
 		set_mode(MODE_LOADING);
 		set_user(user);
 		review_loop(user, "");
 	}
-	
+
 	function review_loop(user, after){
 		$.get("/user/" + user.name + "/overview.json?limit=100&after=" + after).fail(function(){
 			console.log("User " + user.name + " shadowbanned?");
@@ -251,7 +271,7 @@ function ReviewWindow(core){
 				return acc;
 			}, {});
 			// console.log(headers["x-ratelimit-remaining"]);
-			
+
 			// review submissions
 			$.each(d.data.children, function(i, v){
 				// collate post data
@@ -267,17 +287,17 @@ function ReviewWindow(core){
 					post.html = $("<div>").html(data.body_html).text();
 					post.url = data.link_permalink + data.id + "/?context=10000";
 				}
-				
+
 				post.subreddit = data.subreddit;
 				post.created = data.created_utc;
 				post.timestamp = timeConverter(post.created);
 				post.time_ago = timeSince(post.created) + " ago";
 				post.points = data.score;
-				
+
 				// store post on user
 				user.posts.push(post);
 				self.element.find(".total-posts").text(user.posts.length);
-				
+
 				// review post
 				var has_match = false;
 				$.each(core.blacklist, function(ii, pattern){
@@ -288,7 +308,7 @@ function ReviewWindow(core){
 						post.add_word(body_match[0]);
 						user.add_word(body_match[0]);
 					}
-					
+
 					var title_match = pattern.exec(post.title);
 					if (title_match){
 						has_match = true;
@@ -297,7 +317,7 @@ function ReviewWindow(core){
 						user.add_word(title_match[0]);
 					}
 				});
-				
+
 				// match bad subs
 				$.each(BAD_SUBS, function(ii, sub){
 					if (post.subreddit.toLowerCase() == sub.toLowerCase()){
@@ -307,14 +327,14 @@ function ReviewWindow(core){
 						return false;
 					}
 				});
-				
+
 				if (!has_match) return;
-				
+
 				// store match & add to window
 				user.matches.push(post);
 				add_post(user, post);
 			});
-			
+
 			// either stop or keep going
 			after = d.data.after;
 			if (!after || $.isEmptyObject(d.data.children)){
@@ -330,10 +350,10 @@ function ReviewWindow(core){
 			}
 		});
 	}
-	
+
 	function add_post(user, post){
 		var e = self.element;
-		
+
 		var temp = self.post_template.clone();
 		temp.find(".username").text(user.name);
 		temp.find(".points").text(post.points + " point" + (post.points == 1 ? "" : "s"));
@@ -344,12 +364,12 @@ function ReviewWindow(core){
 		temp.find(".post-content").append(post.html);
 		temp.find(".post-url").attr("href", post.url);
 		temp.find(".post-matches").text(post.matches.sort().join(", "));
-		
+
 		e.find(".post-display-container").append(temp);
-		
+
 		// total match count
 		e.find(".total-matches").text(user.matches.length);
-		
+
 		// all matched words
 		var all_matches = [];
 		$.each(user.matched_words, function(k, v){
@@ -361,21 +381,21 @@ function ReviewWindow(core){
 		var match_string = all_matches.map(function(c, i, a){
 			return c[0];
 		});
-		
+
 		e.find(".all-matched-words").html("<b>All matches: </b>" + match_string.join(", "));
 		//e.find(".all-matched-words").html("<b>All matches: </b>" + user.matched_words.sort().join(", "));
 	}
-	
+
 	function set_user(user){
 		self.element.find(".review-username").text(user.name)
 					.attr("href", "/user/" + user.name);
 	}
-	
+
 	function set_mode(mode){
 		self.mode = mode;
 		self.element.find(".review-mode").text(mode);
 	}
-	
+
 	function open(){
 		if (is_open()) return;
 		self.state_open = true;
@@ -386,7 +406,7 @@ function ReviewWindow(core){
 		self.element.find(".total-posts").html("0");
 		$("body").css("overflow", "hidden");
 	}
-	
+
 	function close(){
 		if (!is_open()) return;
 		self.state_open = false;
@@ -394,23 +414,23 @@ function ReviewWindow(core){
 		self.element.find(".post-display-container").html("");
 		$("body").css("overflow", "");
 	}
-	
+
 	function is_open(){
 		return self.state_open;
 	}
-	
+
 	function log(msg){
 		var elem = $("<p class='review-log'>").text(msg);
 		self.element.find(".").append(elem);
 		console.log("[Cheka Log]: " + msg);
 	}
-	
+
 	// create window html and css
 	function init(){
 		var css_variables = {
 			"@text_color": "#111"
 		};
-		
+
 		var css = (function(){/*
 			////////////////////////////////
 			// CHEKA WINDOW
@@ -423,11 +443,11 @@ function ReviewWindow(core){
 				background-color: rgba(0, 0, 0, .8);
 				z-index: 9999;
 			}
-			
+
 			#cheka-review.open{
 				display: block;
 			}
-			
+
 			.cheka-container{
 				position: absolute;
 				top: 20px; bottom: 40px;
@@ -438,11 +458,11 @@ function ReviewWindow(core){
 				z-index: 9999;
 				font-size: 12px;
 			}
-			
+
 			.review-header h1{
 				margin: 0; padding: 0;
 			}
-			
+
 			.review-header .review-close{
 				position: absolute;
 				top: 0; right: 0;
@@ -451,15 +471,15 @@ function ReviewWindow(core){
 				cursor: pointer;
 				color: #555;
 			}
-			
-			
+
+
 			////////////////////////////////
 			// OVERVIEW
 			////////////////////////////////
 			.review-username{
 				font-weight: bold;
 			}
-			
+
 			.review-overview .all-matched-words{
 				display: block;
 				background-color: #fff;
@@ -469,8 +489,8 @@ function ReviewWindow(core){
 				overflow-y: scroll;
 				max-height: 30px;
 			}
-			
-			
+
+
 			////////////////////////////////
 			// POST CONTAINER
 			////////////////////////////////
@@ -483,29 +503,29 @@ function ReviewWindow(core){
 				overflow-y: scroll;
 				padding: 8px;
 			}
-			
+
 			.review-post{
 				padding-bottom: 15px;
 				margin-bottom: 15px;
 				border-bottom: 1px solid #ddd;
 			}
-			
+
 			.post-header{
 				color: #777;
 			}
-			
+
 			.post-header .time-ago{
 				font-size: 10px;
 			}
-			
+
 			.post-header .subreddit, .post-header .points, .post-header .thread-title{
 				font-weight: bold;
 			}
-			
+
 			.post-content{
 				padding: 4px 8px;
 			}
-			
+
 			.review-post .review-highlight{
 				//font-weight: bold;
 				background-color: rgba(255, 60, 0, .5);
@@ -513,7 +533,7 @@ function ReviewWindow(core){
 				padding: 0 2px;
 			}
 		*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
-		
+
 		// replace css variables; remove single line comments
 		$.each(css_variables, function(k, v){
 			var re = new RegExp(k, "g");
@@ -521,7 +541,7 @@ function ReviewWindow(core){
 		});
 		css = css.replace(/\/\/.*/g, ""); // remove "//" comments
 		$("html").append("<style id='cheka-style'>"+css+"</style>");
-		
+
 		// create html
 		var html = (function(){/*
 			<div id="cheka-review">
@@ -548,7 +568,7 @@ function ReviewWindow(core){
 				</div>
 			</div>
 		*/}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
-		
+
 		self.element = $(html);
 		self.element.find(".review-close").click(function(){ self.close(); });
 		self.post_template = self.element.find(".review-post-template").detach().removeClass("review-post-template");
@@ -575,19 +595,19 @@ function timeSince(UNIX_timestamp){
 	var interval = Math.floor(seconds / 31536000);
 
 	if (interval > 1) return interval + " years";
-	
+
 	interval = Math.floor(seconds / 2592000);
 	if (interval > 1) return interval + " months";
-	
+
 	interval = Math.floor(seconds / 86400);
 	if (interval > 1) return interval + " days";
-	
+
 	interval = Math.floor(seconds / 3600);
 	if (interval > 1) return interval + " hours";
-	
+
 	interval = Math.floor(seconds / 60);
 	if (interval > 1) return interval + " minutes";
-	
+
 	return Math.floor(seconds) + " seconds";
 }
 
